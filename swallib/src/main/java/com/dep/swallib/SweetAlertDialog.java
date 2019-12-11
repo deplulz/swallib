f0de580d4fc7103b1d6baf46e4a73b37a5e7ebb9
deplulz <devribudi@gmail.com>
2019-12-11 15:45:49 +0700

    fixing dir class

--- swallib/src/main/java/com/dep/swallib/SweetAlertDialog.java
+++ swallib/src/main/java/com/dep/swallib/SweetAlertDialog.java
@@ -16,7 +16,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.dep.testsweetallert.R;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.List;
